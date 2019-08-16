9504c4f6057439a11f30f6c33472bfe1ca06be64
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-08-16 11:05:30 -0500

    Making a table not sure if this is right but whatever

--- migrations/20190816105243_create-new-table.js
+++ migrations/20190816105243_create-new-table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+return knex.schema.createTable('project', tbl => {
+    tbl.increments();
+    tbl.text("tasks")
+    tbl.text("resource")
+})
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('project');
 };
